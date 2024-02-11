2855fe4dbe38be11e1553156e052b20601bf76d0
Renato Zapata <renato.souza@navvis.com>
2024-02-11 16:21:03 +0100

    evaluation modules added to project

---
+++ modules/evaluate.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3.10
+# -*- coding: utf-8 -*-
+
+import gensim
+import collections
+import random
+
+
+def assess_model(train_corpus: list, model: gensim.models.doc2vec.Doc2Vec) -> None:
+    # Assessing Model
+    ranks = []
+    second_ranks = []
+    for doc_id in range(len(train_corpus)):
+        inferred_vector = model.infer_vector(train_corpus[doc_id].words)
+        sims = model.dv.most_similar([inferred_vector], topn=len(model.dv))
+        rank = [docid for docid, sim in sims].index(doc_id)
+        ranks.append(rank)
+
+        second_ranks.append(sims[1])
+    counter = collections.Counter(ranks)
+    print(counter)
+
+    # Testing Model
+    print('Document ({}): «{}»\n'.format(doc_id, ' '.join(train_corpus[doc_id].words)))
+    print(u'SIMILAR/DISSIMILAR DOCS PER MODEL %s:\n' % model)
+    for label, index in [('MOST', 0), ('SECOND-MOST', 1), ('MEDIAN', len(sims)//2), ('LEAST', len(sims) - 1)]:
+        print(u'%s %s: «%s»\n' % (label, sims[index], ' '.join(train_corpus[sims[index][0]].words)))
+
+    # Pick a random document from the corpus and infer a vector from the model
+    doc_id = random.randint(0, len(train_corpus) - 1)
+
+    # Compare and print the second-most-similar document
+    print('Train Document ({}): «{}»\n'.format(doc_id, ' '.join(train_corpus[doc_id].words)))
+    sim_id = second_ranks[doc_id]
+    print('Similar Document {}: «{}»\n'.format(sim_id, ' '.join(train_corpus[sim_id[0]].words)))
